06b4506e6316225294c7a872dd536cbd18427d9f
22210069 <Qasimmatloob786@gmail.com>
2022-09-30 14:53:32 +0100

    new mod

--- src/App.java
+++ src/App.java
@@ -1,18 +1,32 @@
 import helpers.ConsoleColours;
+import helpers.InputReader;
 
-public class App 
+public class App
 {
-    public static void main(String[] args) throws Exception 
+    static String myName;
+
+    static int bestNumber;
+
+    public static void main(String[] args) throws Exception
     {
         System.out.println(ConsoleColours.ANSI_BRIGHT_BG_PURPLE);
         System.out.println(ConsoleColours.ANSI_BRIGHT_BG_YELLOW);
-        System.out.println(ConsoleColours.CONSOLE_CLEAR);
+        System.out.println(ConsoleColours.ANSI_BLACK);
 
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println("by Mohammad Qasim Matloob");
         System.out.println();
-
+         calculater
     }
+}
+public static void CalculateRectangle()
+{
+int lenght;
+int height;
+
+    System.out.println("caculate Rectangle")
+            lenght = InputReader.getLnt( prompt "please enter the lenght of the Rectangle in cm")
+
 }
